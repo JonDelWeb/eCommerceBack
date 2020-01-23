2d5ca654f0a40037985061e4fa2416bd760c19ef
jonathan delhoux <jonathan.delhoux@gmail.com>
2020-01-23 22:19:58 +0100

    initial commit

---
+++ routes/stuff.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer_config');
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', stuffCtrl.getAllStuff);
+router.get('/account/myStuff', auth, stuffCtrl.getAllStuffByUser);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+module.exports = router;   
